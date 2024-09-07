842e692c4f9ce2b2997d6d4f37e77df5a1d94933
Umar Igan <38042220+UmarIgan@users.noreply.github.com>
2024-09-07 10:04:01 +0000

    updated app

--- utils.py
+++ utils.py
@@ -9,11 +9,11 @@ model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, r
                                              do_sample=True, temperature=0.9,top_p=0.9,top_k=40)
 tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
 
-def process_single_image_and_get_result(image_path, prompt = "Define the image"):
-    image = Image.open(image_path)
-    model_id = "vikhyatk/moondream2"
-    revision = "2024-05-08"
+def process_single_image_and_get_result(image, prompt="Define the image"):
+    # No need to open the image again, it's already opened and passed as an image object
+    image = image.convert('RGB')  # Ensure it's in RGB format
     enc_image = model.encode_image(image)
     result = model.answer_question(enc_image, prompt, tokenizer)
     print("results created")
     return result
+
